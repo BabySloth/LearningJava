b4e3a30c454ff51b89ac9180b04d1dfe5b17b2bd
William Cao <wcao00@stuy.edu>
2019-02-04 11:30:56 -0500

    classwork for 1/4/19

---
+++ AlgorithmsAndDataStructure/Classwork0.java
@@ -0,0 +1,28 @@
+package AlgorithmsAndDataStructure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 1/4/19
+ */
+public class Classwork0 {
+    public static void main(String[] args) {
+        System.out.println(makeAllWords(2, 3));
+    }
+
+    public static List<String> makeAllWords(int k, int maxLetter){
+        return makeAllWords(k, maxLetter, "", new ArrayList<>(maxLetter ^ k));
+    }
+
+    public static List<String> makeAllWords(int k, int maxLetter, String currentString, List<String> combination){
+        if(k == 0){
+            combination.add(currentString);
+        }else{
+            for(int i = 0; i < maxLetter; i++){
+                makeAllWords(k - 1, maxLetter, currentString + ((char) ('a' + i)), combination);
+            }
+        }
+        return combination;
+    }
+}
